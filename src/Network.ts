9c4b0c5b92452a53698bc5417c1d208229406ccf
Hugo Johnson <hugojohnson271@gmail.com>
2024-09-19 16:58:13 +0200

    Fixed .env

--- src/Network.ts
+++ src/Network.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 import { RequestResponse } from "./Interfaces";
 
-export const baseURL = import.meta.env.VITE_PRODUCTION ? "http://localhost:3001/workaholic/" : "https://44.220.164.141.nip.io/workaholic/"
+export const baseURL = import.meta.env.VITE_DEV ? "http://localhost:3001/workaholic/" : "https://44.220.164.141.nip.io/workaholic/"
 
 const apiClient = axios.create({
     baseURL: baseURL,
