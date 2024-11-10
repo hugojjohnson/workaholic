cab25976b1aca0000b352b9ae0b0d543be6ad25b
Hugo Johnson <hugojohnson271@gmail.com>
2024-11-10 10:56:26 +0100

    Added favicon

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
+  base: "./",
   build: {
     target: "esnext",
     outDir: 'build',
