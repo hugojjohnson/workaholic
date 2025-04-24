bfa16698a7bce130623bade44678c3c73ede3462
Hugo Johnson <hugojohnson271@gmail.com>
2025-04-24 13:55:31 +1000

    Fixed image paths

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,11 +19,11 @@ export default function Header(): React.ReactElement {
         }
         <Outlet /> { /** This is where the body of your application will be rendered. **/}
         {
-            user && <div className="w-full h-20 text-xl text-gray-300 bg-[#242424] flex flex-row md:justify-end items-center justify-around md:pr-10 visible md:hidden fixed md:static bottom-0">
-                <Link className={`flex flex-col items-center gap-1 active:scale-75 transition-all ease-in-out duration-100 ${location.pathname === "/" ? "text-white" : "opacity-50"}`} to="/"><img src="/icons/timer.png" className="w-8 invert" /><p className="text-xs">Timer</p></Link>
-                <Link className={`flex flex-col items-center gap-1 active:scale-75 transition-all ease-in-out duration-100 ${location.pathname === "/settings" ? "text-white" : "opacity-50"}`} to="/settings"><img src="/icons/settings.png" className="w-8 invert" /><p className="text-xs">Settings</p></Link>
-                <Link className={`flex flex-col items-center gap-1 active:scale-75 transition-all ease-in-out duration-100 ${location.pathname === "/reports" ? "text-white" : "opacity-50"}`} to="/reports"><img src="/icons/reports.png" className="w-8 invert" /><p className="text-xs">Reports</p></Link>
-                <Link className={`flex flex-col items-center gap-1 active:scale-75 transition-all ease-in-out duration-100 ${location.pathname === "/profile" ? "text-white" : "opacity-50"}`} to="/profile"><img src="/icons/profile.png" className="w-8 invert" /><p className="text-xs">Profile</p></Link>
+            user && <div className="w-full h-20 text-xl text-gray-300 bg-[#242424] flex flex-row md:justify-end items-center justify-around py-3 md:pr-10 visible md:hidden fixed md:static bottom-0">
+                <Link className={`flex flex-col items-center gap-1 active:scale-75 transition-all ease-in-out duration-100 ${location.pathname === "/" ? "text-white" : "opacity-50"}`} to="/"><img src="./icons/timer.png" className="w-8 invert" /><p className="text-xs">Timer</p></Link>
+                <Link className={`flex flex-col items-center gap-1 active:scale-75 transition-all ease-in-out duration-100 ${location.pathname === "/settings" ? "text-white" : "opacity-50"}`} to="/settings"><img src="./icons/settings.png" className="w-8 invert" /><p className="text-xs">Settings</p></Link>
+                <Link className={`flex flex-col items-center gap-1 active:scale-75 transition-all ease-in-out duration-100 ${location.pathname === "/reports" ? "text-white" : "opacity-50"}`} to="/reports"><img src="./icons/reports.png" className="w-8 invert" /><p className="text-xs">Reports</p></Link>
+                <Link className={`flex flex-col items-center gap-1 active:scale-75 transition-all ease-in-out duration-100 ${location.pathname === "/profile" ? "text-white" : "opacity-50"}`} to="/profile"><img src="./icons/profile.png" className="w-8 invert" /><p className="text-xs">Profile</p></Link>
             </div>
             
         }
