2d806b7b2b26fcef384408c6ef0724b49bc1869b
Hugo Johnson <hugojohnson271@gmail.com>
2025-07-27 12:25:06 +1000

    added preferences

---
+++ src/components/auth/SignInButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+
+export default function SignInButton() {
+  const [providers, setProviders] = useState<Record<string, any> | null>(null);
+
+  useEffect(() => {
+    fetch("/api/auth/providers")
+      .then((res) => res.json())
+      .then(setProviders);
+  }, []);
+
+  if (!providers) return <p>Loading sign-in options...</p>;
+
+  return (
+    <div>
+      {Object.values(providers).map((provider) => (
+        <button onClick={() => signIn(provider.id)} className="border-[2px] border-gray-300 inline-block rounded-md px-3 py-3 transition bg-white hover:bg-gray-50">
+          Sign in with {provider.name}
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
+            alt="Google logo"
+            className="inline-block w-5 h-5 ml-5"
+          />
+        </button>
+      ))}
+    </div>
+  );
+}
